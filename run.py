c16d6c86943829d15c0ac094d53a05c589064c46
luke <luke@Desonstar.com>
2019-12-16 15:11:28 +0800

    format fixed

--- run.py
+++ run.py
@@ -29,7 +29,6 @@ for pg in soup.select('.paginator a'):
     plist.append(pg.attrs['href'])
 if  len(plist)>0:
     plist.pop()
-print(plist)
 
 
 # Get the full book list
@@ -48,6 +47,7 @@ with progressbar.ProgressBar(max_value=len(plist)) as bar:
     for pg in plist:
         i = i + 1
         gurl = 'http://book.douban.com/'+pg
+        time.sleep(2)
         html = requests.get(url=gurl,headers={'User-Agent': user_agent}).content
         soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
         for bk in soup.select('.subject-item .info h2  a'):
@@ -67,6 +67,7 @@ with progressbar.ProgressBar(max_value=len(blist)) as bar:
     for bks in blist:
         i=i+1
         gurl = bks[1]
+        time.sleep(2)
         html = requests.get(url=gurl,headers={'User-Agent': user_agent}).content
         soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
         infos = soup.find(id='info')
@@ -95,6 +96,7 @@ print("=======================")
 #http://my1.hzlib.net/opac/search;jsessionid=52BF0FA6791BFB1B1BEE7D49341B5086?&q=9787540493363&searchWay=isbn&sortWay=score&sortOrder=desc&scWay=dim&searchSource=reader
 
 def fetchBook(addr):
+    time.sleep(2)
     html = requests.get(url=addr,headers={'User-Agent': user_agent}).content
     soup = BeautifulSoup(html, 'html.parser', from_encoding='utf-8')
     #import pdb
@@ -111,6 +113,7 @@ def fetchBook(addr):
         for j in bid:
             try:
                 preaddr = "http://my1.hzlib.net/opac/book/holdingPreviews?bookrecnos="+j+"&curLibcodes=&return_fmt=json"
+                time.sleep(2)
                 html = requests.get(url=preaddr,headers={'User-Agent': user_agent}).content
                 import json
                 rec=json.loads(html)['previews'][j]
@@ -141,17 +144,19 @@ for bk in bkdetails:
     bkinfos.append([bookname, bookloc])
 
 # Show Book Rec and Lib Info
-print("书籍借阅信息")
+print("")
+print("")
 for bk in bkinfos:
-    print("")
-    print(">    书名:《"+ bk[0] +"》")    
     if(len(bk[1])==0):
-        print("无在馆信息")    
+        #print("无在馆信息")    
+        continue
     else:
-        print("在馆信息:")    
+        print("|> 书名:《"+ bk[0] +"》")    
+        print("   在馆信息:")    
     for libs in bk[1]:
         for lib in libs:
-            print("    #  借书号:  "+ lib[0])    
-            print("    #  所在地:  "+ lib[2])    
-            print("    #  可出借数量:  %d"%(lib[3]))
+            print("")
+            print("#  借书号:  "+ lib[0])    
+            print("#  所在地:  "+ lib[2])    
+            print("#  可出借数量:  %d"%(lib[3]))
         print("")
